228fdf54cf52513d7e25a7de554760d5cdab8517
Aida Zhdanova <aya.verdi@gmail.com>
2023-06-04 02:32:12 +0300

    fix

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -46,17 +46,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.request.method in SAFE_METHODS:
             return RecipeSerializer
         return RecipeCreateSerializer
-
+    
     @staticmethod
-    def add(request, pk, serializers):
+    def post_method(request, pk, serializers):
         data = {'user': request.user.id, 'recipe': pk}
         serializer = serializers(data=data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+   
     @staticmethod
-    def delete(request, pk, model):
+    def delete_method(request, pk, model):
         user = request.user
         recipe = get_object_or_404(Recipe, id=pk)
         model_obj = get_object_or_404(model, user=user, recipe=recipe)
@@ -66,24 +66,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=["POST"],
             permission_classes=[IsAuthenticated])
     def favorite(self, request, pk):
-        return self.add(
+        return self.post_method(
             request=request, pk=pk, serializers=FavoriteSerializer)
 
     @favorite.mapping.delete
     def delete_favorite(self, request, pk):
-        return self.delete(
+        return self.delete_method(
             request=request, pk=pk, model=Favourite)
 
     @action(detail=True, methods=["POST"],
             permission_classes=[IsAuthenticated])
     def shopping_cart(self, request, pk):
         self.queryset = self.queryset.prefetch_related('shopping')
-        return self.add(
+        return self.post_method(
             request=request, pk=pk, serializers=ShoppingCartSerializer)
 
     @shopping_cart.mapping.delete
     def delete_shopping_cart(self, request, pk):
-        return self.delete(
+        return self.delete_method(
             request=request, pk=pk, model=ShoppingCart)
 
     @action(detail=False, methods=['GET'],
@@ -113,38 +113,48 @@ class RecipeViewSet(viewsets.ModelViewSet):
 class CustomUserViewSet(UserViewSet):
     pagination_class = PageLimitPagination
 
-    @action(
-        methods=['POST', 'DELETE'], detail=True,
-        permission_classes=[IsAuthenticated])
-    def subscribe(self, request, id=None):
-        if request.method == 'POST':
-            user = request.user
-            author = get_object_or_404(User, id=id)
-            if user == author:
-                return Response({
-                    'errors': 'Подписаться на себя не получится!'},
-                    status=status.HTTP_400_BAD_REQUEST)
-            if Subscribe.objects.filter(user=user, author=author).exists():
-                return Response({
-                    'errors': 'Вы уже подписаны на этого пользователя!'},
-                    status=status.HTTP_400_BAD_REQUEST)
+    @staticmethod
+    def post_method(request, id, serializers):
+        user = request.user
+        author = get_object_or_404(User, id=id)
+        if user == author:
+            return Response({
+                'errors': 'Подписаться на себя не получится!'},
+                status=status.HTTP_400_BAD_REQUEST)
+        if Subscribe.objects.filter(user=user, author=author).exists():
+            return Response({
+                'errors': 'Вы уже подписаны на этого пользователя!'},
+                status=status.HTTP_400_BAD_REQUEST)
 
-            follow = Subscribe.objects.create(user=user, author=author)
-            serializer = SubscriptionSerializer(
-                follow, context={'request': request}
-            )
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        elif request.method == 'DELETE':
-            subscriber = request.user
-            author = get_object_or_404(User, id=id)
-            subscription = Subscribe.objects.filter(
-                user=subscriber, author=author)
-            if not subscription.exists():
-                return Response(
-                    {'errors': 'Такой подписки нет!'},
-                    status=status.HTTP_400_BAD_REQUEST)
-            subscription.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+        follow = Subscribe.objects.create(user=user, author=author)
+        serializer = serializers(
+            follow, context={'request': request}
+        )
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @staticmethod
+    def delete_method(request, id):
+        subscriber = request.user
+        author = get_object_or_404(User, id=id)
+        subscription = Subscribe.objects.filter(
+            user=subscriber, author=author)
+        if not subscription.exists():
+            return Response(
+                {'errors': 'Такой подписки нет!'},
+                status=status.HTTP_400_BAD_REQUEST)
+        subscription.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['POST'],
+            detail=True,
+            permission_classes=[IsAuthenticated])
+    def subscribe(self, request, id=None):
+        return self.post_method(
+            request=request, id=id, serializers=SubscriptionSerializer)
+
+    @subscribe.mapping.delete
+    def unsubscribe(self, request, id=None):
+        return self.delete_method(request=request, id=id)
 
     @action(detail=False, permission_classes=[IsAuthenticated])
     def subscriptions(self, request):
